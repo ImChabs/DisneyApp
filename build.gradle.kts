9b6d2070f9ade818e7a7814a73b9eb81852efdce
ImChabs <rubenyebran@gmail.com>
2026-05-31 14:10:47 -0300

    Librerías para compose stability

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.stability.analyzer) apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
